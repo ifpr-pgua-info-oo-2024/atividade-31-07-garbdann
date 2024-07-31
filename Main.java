5f4844fc932e251c77c4a90f844a5e91d46eb692
Daniel Garbeto Cardoso <garbetod@gmail.com>
2024-07-31 11:28:19 -0300

    Atividade 31/07

--- Main.java
+++ Main.java
@@ -1,10 +1,24 @@
 public class Main {
-    
     public static void main(String[] args) {
+        Academia acad1 = new Academia ("World", "Rua José Baka", "34259343");
+        Academia acad2 = new Academia ("Muscle", "Rua Mario Roque Filho", "34210012");
 
-       
-        System.out.println("Faça sua atividade");
- 
+        System.out.println ("\nObjeto 1"); 
+        System.out.println (acad1.toString());
+        System.out.println ("\nObjeto 2");
+        System.out.println (acad2.toString());
+        
+        acad1.setNome ("Aquatikus");
+        acad1.setEndereco ("Rua das Rosas");
+        acad1.setTelefone ("4199512-3213");
+
+        acad2.setNome ("Worship");
+        acad2.setEndereco ("Rua das Calêndulas");
+        acad2.setTelefone ("4192131-4239");
+
+        System.out.println ("\nObjeto 1");
+        System.out.println (acad1.toString());
+        System.out.println ("\nObjeto 2");
+        System.out.println (acad2.toString());
     }
- 
 }
