fecd696eb03336dd8d9216ddac8d36c104182e86
Daniel Garbeto Cardoso <garbetod@gmail.com>
2024-08-14 11:41:28 -0300

    14/08

--- Aluno.java
+++ Aluno.java
@@ -1,85 +1,30 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.text.ParseException;
 
-public class Aluno {
-    private String nome;
+public class Aluno extends Pessoa {
     private String nivel;
-    private Date nascimento;
-    private String genero;
-    private double altura;
-    private double peso;
 
-    public Aluno(String nome, String nivel, Date nascimento, String genero, double altura, double peso) {
-        this.nome = nome;
+    public Aluno(String nome, String nivel, String nascimento, String genero, double altura, double peso)
+            throws ParseException {
+        super(nome, nascimento, genero, altura, peso);
         this.nivel = nivel;
-        this.nascimento = nascimento;
-        this.genero = genero;
-        this.altura = altura;
-        this.peso = peso;
     }
 
-    public Aluno(String nome, String nivel, Date nascimento) {
+    public Aluno(String nome, String nivel, String nascimento) throws ParseException {
         this(nome, nivel, nascimento, "", 0, 0);
-    }
-
-    public String getNome() {
-        return nome;
     }
 
     public String getNivel() {
         return nivel;
     }
 
-    public Date getDataNascimento() {
-        return nascimento;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public double getAltura() {
-        return altura;
-    }
-
-    public double getPeso() {
-        return peso;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
     public void setNivel(String nivel) {
         this.nivel = nivel;
     }
 
-    public void setNascimento(Date nascimento) {
-        this.nascimento = nascimento;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public void setAltura(double altura) {
-        this.altura = altura;
-    }
-
-    public void setPeso(double peso) {
-        this.peso = peso;
-    }
-
     @Override
     public String toString() {
-        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("dd/MM/yyyy");
-
-        return "\nNome: " + nome +
-                "\nNível: " + nivel +
-                "\nData de nascimento: " + simpleDateFormat.format(nascimento) +
-                "\nGênero: " + genero +
-                "\nAltura: " + altura +
-                "\nPeso: " + peso;
+        return super.toString() +
+                "\nNível: " + nivel;
     }
 
 }
